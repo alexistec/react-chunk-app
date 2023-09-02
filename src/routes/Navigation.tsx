5cdadc542c6eeeb81eb6339739e23ae1e3927f75
aaquinonez <aaquinonez@teco.com.ar>
2023-09-02 18:01:25 -0300

    It's basic config

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,45 +1,74 @@
-import { Suspense } from 'react'
-import { BrowserRouter, NavLink, Navigate, Route, Routes } from 'react-router-dom'
-import { routes } from './routes'
-
-
-
-
-export const Navigation = () => {
-  return (
-    <Suspense fallback={ <span>loading</span> }>
-        <BrowserRouter>
-            <div className='main-layout'>
-                <nav>
-                    <ul>
-                        {
-                            routes.map( ({ name, to }) => (
-                                <li key={ to }>
-                                    <NavLink 
-                                        to={ to } 
-                                        className={ ({ isActive }) => isActive ? 'nav-active' :''  }>
-                                        { name }
-                                    </NavLink>
-                                </li>
-                            ))
-                        }
-                        
-                    </ul>
-                </nav>
-                <Routes>
-                    {
-                        routes.map( ({ path, Component }) => (
-                            <Route
-                                key={ path } 
-                                path={ path }  
-                                element={ <Component/> }
-                            />
-                        ))
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    NavLink
+  } from 'react-router-dom';
+  
+  import logo from '../logo.svg';
+  
+  export const Navigation = () => {
+    return (
+      <Router>
+        <div className="main-layout">
+          <nav>
+              <img src={ logo } alt="React Logo" />
+            <ul>
+              <li>
+                <NavLink 
+                    to="/" 
+                    className={({ isActive }) =>
+                        [
+                        "link_nav",
+                            isActive ? "nav-active" : null,
+                        ]
+                        .filter(Boolean)
+                        .join(" ")
                     }
-                    <Route path='/*' element={ <Navigate to={ routes[0].to } replace /> } />
-                </Routes>
-            </div>
-        </BrowserRouter>
-    </Suspense>
-  )
-}
+                    end
+                >
+                    Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink 
+                    to="/about" 
+                    className={({ isActive }) =>
+                        [
+                        "link_nav",
+                            isActive ? "nav-active" : null,
+                        ]
+                        .filter(Boolean)
+                        .join(" ")
+                    }
+                    end
+                    >About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/users" 
+                    className={({ isActive }) =>
+                    [
+                    "link_nav",
+                        isActive ? "nav-active" : null,
+                    ]
+                    .filter(Boolean)
+                    .join(" ")
+                }
+                end
+                >Users</NavLink>
+              </li>
+            </ul>
+          </nav>
+  
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Routes>
+            <Route path="/about" element={<h1>About</h1>} />
+            <Route path="/users" element={<h1>Users</h1>} />
+            <Route path="/" element={<h1>Home</h1>} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
